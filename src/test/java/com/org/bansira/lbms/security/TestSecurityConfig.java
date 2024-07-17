0b429dece0882d7f63016dd874c1a3744d22f151
sreeram3449 <sreeram3449@gmail.com>
2024-07-17 15:11:47 +0530

    Spring Security configured and csrf disabled

--- src/test/java/com/org/bansira/lbms/security/TestSecurityConfig.java
+++ src/test/java/com/org/bansira/lbms/security/TestSecurityConfig.java
@@ -1,4 +1,23 @@
 package com.org.bansira.lbms.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().permitAll()  // Allow all requests
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 }
